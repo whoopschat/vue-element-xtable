1c51842cb3e16fdcf2ba0e0687dfe816571d8461
liangmayong <ibeam@qq.com>
2022-08-23 09:36:18 +0800

    v1.1.11

--- src/packages/tab/_tab.vue
+++ src/packages/tab/_tab.vue
@@ -11,8 +11,8 @@
         <template v-if="tab.component">
           <component
             :is="tab.component"
-            :params="tab.params"
-            :query="tab.query"
+            :params="tab.params || params"
+            :query="tab.query || query"
           />
         </template>
         <div v-else>
@@ -36,12 +36,46 @@ export default {
       default: () => {
         return [
           {
-            key: "tab1",
+            name: "tab1",
             label: "Tab 1",
+          },
+          {
+            name: "tab2",
+            label: "Tab 2",
           },
         ];
       },
     },
+    params: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+    query: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+  watch: {
+    tabs() {
+      this.refreshActiveName();
+    },
+  },
+  methods: {
+    refreshActiveName() {
+      let names = (this.tabs || []).map((tab) => {
+        return tab.name;
+      });
+      if (!this.activeName || names.indexOf(this.activeName) < 0) {
+        this.activeName = names[0];
+      }
+    },
+  },
+  created() {
+    this.refreshActiveName();
   },
 };
 </script>

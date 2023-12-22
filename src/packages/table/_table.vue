c2e13d180e7e6fb319467891541af3f8a2e32fbf
iqudoo <iqudoo@qq.com>
2023-12-22 15:38:42 +0800

    v1.2.60

--- src/packages/table/_table.vue
+++ src/packages/table/_table.vue
@@ -1,221 +1,233 @@
 <template>
   <div v-loading="fetchLoading" class="xTable">
-    <el-alert
-      type="info"
-      v-if="filterCount > 0"
-      style="margin-bottom: 10px"
-      :title="filterTips"
-      :closable="false"
-    >
-    </el-alert>
-    <div @keyup.enter="fetchList">
-      <el-form :inline="true" :size="size || $xUISize" @submit.native.prevent>
-        <el-form-item
-          v-if="
-            showRefreshBtn ||
-            filterList(paramList).length > 0 ||
-            filterList(btnList).length > 0
-          "
-        >
-          <div class="x-table-param-list">
-            <div
-              class="x-table-param-item"
-              v-for="(param, i) in filterList(paramList)"
-              v-show="getValue('show', param, null, true)"
-              :key="'x-table-param-' + i"
-            >
-              <x-input
-                v-model="param.value"
-                @change="handleChangeParams()"
-                @clear="handleClearParam(param)"
-                :disabled="getValue('disabled', param)"
-                :clearable="!!(param.value != getValue('default', param))"
-                :size="getValue('size', param) || size || $xUISize"
-                :styleValue="getValue('styleValue', param) || 'width: 150px;'"
-                :options="getParamOptions(param)"
-              />
+    <div :style="styleVar">
+      <el-alert
+        type="info"
+        v-if="filterCount > 0"
+        style="margin-bottom: 10px"
+        :title="filterTips"
+        :closable="false"
+      >
+      </el-alert>
+      <div @keyup.enter="fetchList">
+        <el-form :inline="true" :size="size || $xUISize" @submit.native.prevent>
+          <el-form-item
+            v-if="
+              showRefreshBtn ||
+              filterList(paramList).length > 0 ||
+              filterList(btnList).length > 0
+            "
+          >
+            <div class="x-table-param-list">
+              <div
+                class="x-table-param-item"
+                v-for="(param, i) in filterList(paramList)"
+                v-show="getValue('show', param, null, true)"
+                :key="'x-table-param-' + i"
+              >
+                <x-input
+                  v-model="param.value"
+                  @change="handleChangeParams()"
+                  @clear="handleClearParam(param)"
+                  :disabled="getValue('disabled', param)"
+                  :clearable="!!(param.value != getValue('default', param))"
+                  :size="getValue('size', param) || size || $xUISize"
+                  :styleValue="getValue('styleValue', param) || 'width: 150px;'"
+                  :options="getParamOptions(param)"
+                />
+              </div>
+              <slot name="param-extra" :refresh="fetchList"></slot>
+              <el-button
+                v-if="showRefreshBtn"
+                :size="size || $xUISize"
+                :loading="fetchLoading"
+                class="x-table-param-item"
+                icon="el-icon-search"
+                @click="fetchList"
+              >
+                {{ refreshLabel }}
+              </el-button>
+              <el-button
+                v-for="(btn, i) in filterList(btnList)"
+                v-show="getValue('show', btn, null, true)"
+                class="x-table-param-item"
+                :key="'x-table-btn-' + i"
+                :loading="getValue('loading', btn)"
+                :disabled="getValue('disabled', btn)"
+                :icon="getValue('icon', btn)"
+                :type="getValue('type', btn)"
+                :style="getValue('styleValue', btn)"
+                :size="getValue('size', btn) || size || $xUISize"
+                @click="callEvent('click', btn, dataParams)"
+              >
+                {{ getValue("label", btn) }}
+              </el-button>
             </div>
-            <slot name="param-extra" :refresh="fetchList"></slot>
-            <el-button
-              v-if="showRefreshBtn"
-              :size="size || $xUISize"
-              :loading="fetchLoading"
-              class="x-table-param-item"
-              icon="el-icon-search"
-              @click="fetchList"
-            >
-              {{ refreshLabel }}
-            </el-button>
-            <el-button
-              v-for="(btn, i) in filterList(btnList)"
-              v-show="getValue('show', btn, null, true)"
-              class="x-table-param-item"
-              :key="'x-table-btn-' + i"
-              :loading="getValue('loading', btn)"
-              :disabled="getValue('disabled', btn)"
-              :icon="getValue('icon', btn)"
-              :type="getValue('type', btn)"
-              :style="getValue('styleValue', btn)"
-              :size="getValue('size', btn) || size || $xUISize"
-              @click="callEvent('click', btn, dataParams)"
-            >
-              {{ getValue("label", btn) }}
-            </el-button>
-          </div>
-        </el-form-item>
-        <slot name="extra" :refresh="fetchList"></slot>
-      </el-form>
-    </div>
-    <el-table
-      ref="xTableEl"
-      :border="border"
-      :data="filterData"
-      :size="size || $xUISize"
-      :highlight-current-row="highlightCurrentRow"
-      :show-header="headerShow"
-      :empty-text="emptyLabel"
-      :height="height"
-      v-if="!fetchErrorMsg"
-      @sort-change="handleSortChange"
-      @current-change="handleCurrentChange"
-      @selection-change="handleSelectionChange"
-    >
-      <el-table-column
-        v-if="selection"
-        type="selection"
-        width="40"
-      ></el-table-column>
-      <el-table-column
-        v-show="field"
-        v-for="(field, i) in filterList(fieldList)"
-        :key="'x-table-column-' + i"
-        :label="getValue('label', field)"
-        :width="getValue('width', field)"
-        :min-width="getValue('minWidth', field)"
-        :sortable="getValue('sortKey', field) ? 'custom' : null"
-        :fixed="getValue('fixed', field)"
-        :prop="getValue('sortKey', field)"
-      >
-        <template slot-scope="scope" v-if="field">
-          <slot
-            v-if="field.slot"
-            :name="'row-' + field.slot"
-            :row="scope.row"
-          ></slot>
-          <span
-            :style="getValue('styleValue', field, scope.row)"
-            :class="getValue('className', field, scope.row)"
-            @click="callEvent('click', field, scope.row)"
-          >
-            <template v-if="getValue('type', field, scope.row) == 'html'">
-              <div v-html="getValue('render', field, scope.row)"></div>
-            </template>
-            <template v-else-if="getValue('type', field, scope.row) == 'link'">
-              <el-link
-                :size="getValue('size', field, scope.row) || size || $xUISize"
-                :disabled="getValue('disabled', field, scope.row)"
-                :type="getValue('linkType', field, scope.row)"
-              >
-                {{ getValue("render", field, scope.row) }}
-              </el-link>
-            </template>
-            <template
-              v-else-if="getValue('type', field, scope.row) == 'button'"
-            >
-              <el-button
-                :style="getValue('styleValue', field, scope.row)"
-                :size="getValue('size', field, scope.row) || size || $xUISize"
-                :disabled="getValue('disabled', field, scope.row)"
-                :type="getValue('buttonType', field, scope.row)"
-              >
-                {{ getValue("render", field, scope.row) }}
-              </el-button>
-            </template>
-            <template v-else-if="getValue('type', field, scope.row) == 'tag'">
-              <el-tag
-                v-if="
-                  getValue('render', field, scope.row) ||
-                  getValue('label', field, scope.row)
-                "
-                :style="getValue('styleValue', field, scope.row)"
-                :type="getValue('tagType', field, scope.row)"
-                :size="getValue('size', field, scope.row) || size || $xUISize"
-              >
-                {{ getValue("render", field, scope.row) }}
-              </el-tag>
-            </template>
-            <span v-else :style="getValue('styleValue', field, scope.row)">
-              {{ getValue("render", field, scope.row) }}
-            </span>
-            <slot :row="scope.row" :field="field" :index="scope.$index"></slot>
-          </span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        v-if="actionList.length > 0"
-        :width="actionWidth"
-        :label="actionLabel"
-        :fixed="actionFixed"
-      >
-        <template slot-scope="scope">
-          <slot name="action" :row="scope.row"></slot>
-          <span
-            :key="'x-table-action-' + i"
-            v-for="(action, i) in filterList(actionList, scope.row)"
-            v-show="getValue('show', action, scope.row, true)"
-          >
-            <template v-if="getValue('type', action, scope.row) == 'button'">
-              <el-button
-                :style="getValue('styleValue', action, scope.row)"
-                :size="getValue('size', action, scope.row) || size || $xUISize"
-                :disabled="getValue('disabled', action, scope.row, false)"
-                :type="getValue('buttonType', action, scope.row)"
-                @click="callEvent('click', action, scope.row)"
-              >
-                {{ getValue("label", action, scope.row) }}
-              </el-button>
-            </template>
-            <template v-else>
-              <el-link
-                class="x-table-action-link"
-                :style="getValue('styleValue', action, scope.row)"
-                :size="getValue('size', action, scope.row) || size || $xUISize"
-                :disabled="getValue('disabled', action, scope.row)"
-                :type="getValue('linkType', action, scope.row) || 'primary'"
-                @click="callEvent('click', action, scope.row)"
-              >
-                {{ getValue("label", action, scope.row) }}
-              </el-link>
-            </template>
-          </span>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-empty
-      v-else-if="!fetchLoading"
-      :description="fetchErrorMsg"
-      :image-size="100"
-    >
-      <el-button
-        v-if="fetchErrorMsg"
-        @click="fetchList"
+          </el-form-item>
+          <slot name="extra" :refresh="fetchList"></slot>
+        </el-form>
+      </div>
+      <el-table
+        ref="xTableEl"
+        :border="border"
+        :data="filterData"
         :size="size || $xUISize"
+        :highlight-current-row="highlightCurrentRow"
+        :show-header="headerShow"
+        :empty-text="emptyLabel"
+        :height="height"
+        v-if="!fetchErrorMsg"
+        @sort-change="handleSortChange"
+        @current-change="handleCurrentChange"
+        @selection-change="handleSelectionChange"
       >
-        {{ retryLabel }}
-      </el-button>
-    </el-empty>
-    <br />
-    <div class="block" v-if="paginationShow">
-      <el-pagination
-        v-if="currentTotal > 0"
-        @current-change="handlePageChange"
-        @size-change="handleSizeChange"
-        :total="currentTotal"
-        :page-size="currentSize"
-        :page-sizes="paginationPageSizes"
-        :current-page="currentPage"
-        :layout="paginationLayout"
-      ></el-pagination>
+        <el-table-column
+          v-if="selection"
+          type="selection"
+          width="40"
+        ></el-table-column>
+        <el-table-column
+          v-show="field"
+          v-for="(field, i) in filterList(fieldList)"
+          :key="'x-table-column-' + i"
+          :label="getValue('label', field)"
+          :width="getValue('width', field)"
+          :min-width="getValue('minWidth', field)"
+          :sortable="getValue('sortKey', field) ? 'custom' : null"
+          :fixed="getValue('fixed', field)"
+          :prop="getValue('sortKey', field)"
+        >
+          <template slot-scope="scope" v-if="field">
+            <slot
+              v-if="field.slot"
+              :name="'row-' + field.slot"
+              :row="scope.row"
+            ></slot>
+            <span
+              :style="getValue('styleValue', field, scope.row)"
+              :class="getValue('className', field, scope.row)"
+              @click="callEvent('click', field, scope.row)"
+            >
+              <template v-if="getValue('type', field, scope.row) == 'html'">
+                <div v-html="getValue('render', field, scope.row)"></div>
+              </template>
+              <template
+                v-else-if="getValue('type', field, scope.row) == 'link'"
+              >
+                <el-link
+                  :size="getValue('size', field, scope.row) || size || $xUISize"
+                  :disabled="getValue('disabled', field, scope.row)"
+                  :type="getValue('linkType', field, scope.row)"
+                >
+                  {{ getValue("render", field, scope.row) }}
+                </el-link>
+              </template>
+              <template
+                v-else-if="getValue('type', field, scope.row) == 'button'"
+              >
+                <el-button
+                  :style="getValue('styleValue', field, scope.row)"
+                  :size="getValue('size', field, scope.row) || size || $xUISize"
+                  :disabled="getValue('disabled', field, scope.row)"
+                  :type="getValue('buttonType', field, scope.row)"
+                >
+                  {{ getValue("render", field, scope.row) }}
+                </el-button>
+              </template>
+              <template v-else-if="getValue('type', field, scope.row) == 'tag'">
+                <el-tag
+                  v-if="
+                    getValue('render', field, scope.row) ||
+                    getValue('label', field, scope.row)
+                  "
+                  :style="getValue('styleValue', field, scope.row)"
+                  :type="getValue('tagType', field, scope.row)"
+                  :size="getValue('size', field, scope.row) || size || $xUISize"
+                >
+                  {{ getValue("render", field, scope.row) }}
+                </el-tag>
+              </template>
+              <span v-else :style="getValue('styleValue', field, scope.row)">
+                {{ getValue("render", field, scope.row) }}
+              </span>
+              <slot
+                :row="scope.row"
+                :field="field"
+                :index="scope.$index"
+              ></slot>
+            </span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          v-if="actionList.length > 0"
+          :width="actionWidth"
+          :label="actionLabel"
+          :fixed="actionFixed"
+        >
+          <template slot-scope="scope">
+            <slot name="action" :row="scope.row"></slot>
+            <span
+              :key="'x-table-action-' + i"
+              v-for="(action, i) in filterList(actionList, scope.row)"
+              v-show="getValue('show', action, scope.row, true)"
+            >
+              <template v-if="getValue('type', action, scope.row) == 'button'">
+                <el-button
+                  :style="getValue('styleValue', action, scope.row)"
+                  :size="
+                    getValue('size', action, scope.row) || size || $xUISize
+                  "
+                  :disabled="getValue('disabled', action, scope.row, false)"
+                  :type="getValue('buttonType', action, scope.row)"
+                  @click="callEvent('click', action, scope.row)"
+                >
+                  {{ getValue("label", action, scope.row) }}
+                </el-button>
+              </template>
+              <template v-else>
+                <el-link
+                  class="x-table-action-link"
+                  :style="getValue('styleValue', action, scope.row)"
+                  :size="
+                    getValue('size', action, scope.row) || size || $xUISize
+                  "
+                  :disabled="getValue('disabled', action, scope.row)"
+                  :type="getValue('linkType', action, scope.row) || 'primary'"
+                  @click="callEvent('click', action, scope.row)"
+                >
+                  {{ getValue("label", action, scope.row) }}
+                </el-link>
+              </template>
+            </span>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-empty
+        v-else-if="!fetchLoading"
+        :description="fetchErrorMsg"
+        :image-size="100"
+      >
+        <el-button
+          v-if="fetchErrorMsg"
+          @click="fetchList"
+          :size="size || $xUISize"
+        >
+          {{ retryLabel }}
+        </el-button>
+      </el-empty>
+      <br />
+      <div class="block" v-if="paginationShow">
+        <el-pagination
+          v-if="currentTotal > 0"
+          @current-change="handlePageChange"
+          @size-change="handleSizeChange"
+          :total="currentTotal"
+          :page-size="currentSize"
+          :page-sizes="paginationPageSizes"
+          :current-page="currentPage"
+          :layout="paginationLayout"
+        ></el-pagination>
+      </div>
     </div>
   </div>
 </template>
@@ -237,7 +249,7 @@
   }
 
   .el-table td.el-table__cell {
-    border-bottom: 0px solid #ebeef5;
+    border-bottom: var(--x-table-border-bottom) solid #ebeef5;
   }
 
   .el-table th.el-table__cell.is-leaf {
@@ -459,6 +471,11 @@ export default {
     }
   },
   computed: {
+    styleVar() {
+      return {
+        "--x-table-border-bottom": this.border ? "1px" : "0px"
+      }
+    },
     filterTips() {
       if (this.filterCount > 0) {
         if (
